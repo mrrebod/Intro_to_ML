42bad2b5d4c161ac59aefda900d410830e9dd0aa
Marco Dober <marco.dober@hotmail.com>
2020-03-11 16:21:57 +0100

    Tried a lot of stuff

--- Task1a/main.py
+++ Task1a/main.py
@@ -8,21 +8,32 @@ Vukasin Lalic  & Marco Dober aka Snorlax
 import numpy as np    
 from sklearn.model_selection import KFold 
 from sklearn.linear_model import Ridge
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import PowerTransformer
+
 
 # Read in complete train set from csv file
 data_set = np.genfromtxt('train.csv', delimiter=',', skip_header=1)
 # Extract ID from train set
 Id = data_set[:, 0]
 # Extract y label from train set
-y = data_set[:, 1]
+y = data_set[:, 1]  
 # Extract feature from train set
 X = data_set[:, 2:]
+
+# Standardize Data 
+"""
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+"""
 
 # lambda
 lamb = np.array([0.01, 0.1, 1, 10, 100])
 
 # Split data set into train and test set
-kf = KFold(n_splits=10)
+kf = KFold(n_splits=10, shuffle=True)
 print('Number of splits: ', kf.get_n_splits(X))
 
 
@@ -34,8 +45,15 @@ for i in range(len(lamb)):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         
-        clf.fit(X_train,y_train)
-        y_predict = clf.predict(X_test)
+        # Standardize Data 
+        scaler = StandardScaler()
+        #scaler = RobustScaler()
+        # scaler = PowerTransformer()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.fit_transform(X_test)
+        
+        clf.fit(X_train_scaled,y_train)
+        y_predict = clf.predict(X_test_scaled)
         
         rmse_batch = np.sqrt(np.mean((y_test-y_predict)**2))
         
