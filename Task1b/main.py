42b9cb81f2e65926ba0195c9aa75df803e56fc77
Marco Dober <marco.dober@hotmail.com>
2020-03-20 12:10:18 +0100

    Deleted StandardScaler from main.py

--- Task1b/main.py
+++ Task1b/main.py
@@ -7,7 +7,6 @@ Vukasin Lalic  & Marco Dober aka Snorlax
 
 import numpy as np  
 from sklearn.linear_model import RidgeCV   
-from sklearn.preprocessing import StandardScaler
 
 # Read in complete train set from csv file
 train_set = np.genfromtxt('train.csv', delimiter=',', skip_header=1)
@@ -18,37 +17,30 @@ y_train = train_set[:, 1]
 # Extract feature from train set
 X_train = train_set[:, 2:]
 
-# Standardize Data
-# initialize scaler 
-scaler = StandardScaler()
-# applay standardization
-X_train_stand = scaler.fit_transform(X_train)
-# X_train_stand = X_train
-
-# compute Phi with standardized features 
-Phi_train_stand = np.zeros((np.shape(X_train)[0], 21))
+# compute Phi  
+Phi_train = np.zeros((np.shape(X_train)[0], 21))
 # Phi 0-4
-Phi_train_stand[:, 0:5] = X_train_stand
+Phi_train[:, 0:5] = X_train
 # Phi 5-9
-Phi_train_stand[:, 5:10] = np.square(X_train_stand)
+Phi_train[:, 5:10] = np.square(X_train)
 # Phi 10-14
-Phi_train_stand[:, 10:15] = np.exp(X_train_stand)
+Phi_train[:, 10:15] = np.exp(X_train)
 # Phi 15-19
-Phi_train_stand[:, 15:20] = np.cos(X_train_stand)
+Phi_train[:, 15:20] = np.cos(X_train)
 # Phi 21
-Phi_train_stand[:, 20] = np.ones(np.shape(X_train_stand)[0])
+Phi_train[:, 20] = np.ones(np.shape(X_train)[0])
 
-# Compute ridge regression for standardized data 
-clf = RidgeCV(alphas=[0.1, 1, 10, 100, 1000, 10000], fit_intercept=False)
-clf.fit(Phi_train_stand, y_train)
+# Compute ridge regression
+clf_Ridge = RidgeCV(alphas=[0.1, 1, 10, 100, 1000, 10000], fit_intercept=False, cv=10)
+clf_Ridge.fit(Phi_train, y_train)
 
 # save coefficients into array
-weights_ridge_stand = clf.coef_
+weights_Ridge = clf_Ridge.coef_
 
 # print some parameter of model
-print(weights_ridge_stand)
-print(clf.alpha_)
-print(clf.score(Phi_train_stand, y_train))
+print('Ridge weights: ', weights_Ridge)
+print('Ridge alpha: ', clf_Ridge.alpha_)
+print('Ridge score: ', clf_Ridge.score(Phi_train, y_train))
 
 # save data
-np.savetxt('submission.csv', weights_ridge_stand)
+np.savetxt('submission.csv', weights_Ridge)
