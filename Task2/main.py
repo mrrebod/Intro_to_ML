bb5a326ec0eabcf93ba33c09805998a4594f8017
Vukasin <vlalic@student.ethz.ch>
2020-04-07 14:35:31 +0200

    Added distance and confidence for task 1

--- Task2/main.py
+++ Task2/main.py
@@ -12,6 +12,10 @@ from sklearn.svm import SVC                # Try out later
 from sklearn.impute import SimpleImputer   # Maybe use this for incomplete data
 from sklearn.multiclass import OneVsRestClassifier
 
+# -----------------------------------------------------------------------------
+
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
 
 # -----------------------------------------------------------------------------
 
@@ -30,12 +34,16 @@ train_labels   = train_labels_df.to_numpy()
 # Create a 3D array where the third dimension is the patient and the first two
 # dimesions are all its values and measurements over time (with pid removed)
 patients_data = np.zeros((12, train_features.shape[1]-1, int(train_features.shape[0]/12) ))
+test_data     = np.zeros((12, test_features.shape[1]-1, int(test_features.shape[0]/12) ))
 for i in range(len(train_labels)):
     patients_data[:,:,i] = train_features[i*12 : 12*(i+1) :1, 1:]
+for i in range(int(len(test_features)/12)):
+    test_data[:,:,i]     = test_features[i*12 : 12*(i+1) :1, 1:]
 
 # Use a vector to save the corresponding PIDs at the corresponding index
 # e.g. pid: 10002 is at index 5, the same as in patients_data[:,:,5]
 train_pid = train_labels[:,0]
+test_pid  = test_features[0::12,0] 
 
 
 # -----------------------------------------------------------------------------
@@ -48,6 +56,7 @@ train_pid = train_labels[:,0]
 
 # initialize vectorized patients_data
 patients_data_vector = np.zeros((patients_data.shape[2], patients_data.shape[0]*patients_data.shape[1]))
+test_data_vector     = np.zeros((test_data.shape[2], test_data.shape[0]*test_data.shape[1]))
 
 for i in range(patients_data.shape[2]):
     patients_data_try = patients_data[:,:,i]
@@ -81,8 +90,42 @@ for i in range(patients_data.shape[2]):
     patients_data_vector[i,:] = np.ndarray.flatten(patients_data_try)
     
     
+# Same for test data ----------------------------------------------------------
+for i in range(test_data.shape[2]):
+    test_data_try = test_data[:,:,i]
+    
+    # If 6 or more entries of a column are not nan and at least one is missing 
+    # -> fill the rest with the median
+    number_of_not_nans = np.count_nonzero(~np.isnan(test_data_try), axis=0) 
+    columns_to_fill = np.greater_equal(number_of_not_nans, 1) & np.less(number_of_not_nans, 12)
+    
+    # Compute the median where needed
+    where_to_fill  = test_data_try[:,np.where(columns_to_fill)]
+    where_to_fill  = np.squeeze(where_to_fill)   # from 3d to 2d
+    median_to_fill = np.nanmedian(where_to_fill,axis=0)
+    
+    #Find indicies that you need to replace
+    inds = np.where(np.isnan(where_to_fill))
+    
+    #Place column means in the indices. Align the arrays using take
+    if np.isscalar(median_to_fill):
+        where_to_fill[inds] = median_to_fill
+    else:
+        where_to_fill[inds] = np.take(median_to_fill, inds[1])
+    
+    
+    # Rewerite the starting array
+    test_data_try[:,np.squeeze(np.where(columns_to_fill))] = where_to_fill
+    
+   #  test_data[:,:,i] = test_data_try
+    
+    # vectorize patients_data
+    test_data_vector[i,:] = np.ndarray.flatten(test_data_try)
+    
+    
 # Replace nan values with 0
 patients_data_vector = np.nan_to_num(patients_data_vector, nan=0)
+test_data_vector = np.nan_to_num(test_data_vector, nan=0)
     
 
 # What to do with all nan columns or columns where nan is in more than 6 entries?
@@ -92,13 +135,16 @@ patients_data_vector = np.nan_to_num(patients_data_vector, nan=0)
 
 # Train the model 
 # clf = LinearSVC(random_state=0, tol=1e-5, class_weight= 'balanced')
-clf = OneVsRestClassifier(SVC(kernel='linear', probability=True, class_weight='balanced'))
+# clf = OneVsRestClassifier(SVC(kernel='linear', probability=True, class_weight='balanced'),n_jobs=-1)
+clf = OneVsRestClassifier(SVC(class_weight='balanced'), n_jobs=-1)
 
-clf.fit(patients_data_vector, train_labels[:, 1:11])
+# clf.fit(patients_data_vector[0:500,:], train_labels[0:500, 1:11])
+clf.fit(patients_data_vector[0:50,:], train_labels[0:50, 1:11])
 
+predict_labels = clf.predict(test_data_vector)
+predict_distance = clf.decision_function(test_data_vector)
+predict_confidence = sigmoid(predict_distance)
 
-# for i in range(patients_data.shape[2]):
-#     clf.fit(patients_data[:,:,i], train_labels[i,1:])
     
 
 # -----------------------------------------------------------------------------
@@ -121,5 +167,8 @@ clf.fit(patients_data_vector, train_labels[:, 1:11])
 # Subtask 3
 # Predict future mean values of key vital signs
 # LABEL_RRate, LABEL_ABPm, LABEL_SpO2, LABEL_Heartrate
+
+
+
 
 
